f274570aa35a8046661323f92db8eb6cb6b1d9d1
Masao Maeda <brt.river@gmail.com>
2011-05-17 22:06:48 +0900

    updated: all contents

--- index.php
+++ index.php
@@ -1,7 +1,15 @@
 <?php
-require_once __DIR__.'/silex.phar';
+// 1-1. phar が利用できる環境の場合は silex.phar を読み込み
+//require_once __DIR__.'/silex.phar';
+// 1-2. phar が利用できない環境の場合は ソースを直接読み込み
+require_once __DIR__.'/Silex.git/autoload.php';
 
 $app = new Silex\Application();
+
+$app->register(new Silex\Extension\TwigExtension(), array(
+    'twig.path'       => __DIR__.'/views',
+    'twig.class_path' => __DIR__.'/vendor/Twig.git/lib',
+));
 
 // Hello Your Name
 $app->get('/hello/{name}', function ($name) {
@@ -9,8 +17,8 @@ $app->get('/hello/{name}', function ($name) {
 });
 
 // index
-$app->get('/', function () {
-    return "This site is Silex Users JP.";
+$app->get('/', function () use($app) {
+    return $app['twig']->render('index.twig');
 });
 
 $app->run();
